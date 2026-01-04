2b60a430aa7c88bc2902721fb2fc5d7321821db3
Harrhy Saladagu <harrhys@gmail.com>
2026-01-04 07:32:22 +0530

    Added RN Apps

---
+++ 03_FRAMEWORKS/REACTNATIVE/ReactNativeApp-1/windows/MyApp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::MyApp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::MyApp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
